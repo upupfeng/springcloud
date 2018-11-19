e44d6643b9ca186927b5178316aea0ae93e1420a
upupfeng <69676213@qq.com>
2018-11-19 17:02:46 +0800

    .,

---
+++ eureka_server/src/main/java/com/mwf/StarterEurekaServer.java
@@ -0,0 +1,15 @@
+package com.mwf;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@SpringBootApplication
+@EnableEurekaServer
+public class StarterEurekaServer {
+	public static void main(String[] args) {
+		SpringApplication.run(StarterEurekaServer.class, args);
+	}
+}
+
+ 
